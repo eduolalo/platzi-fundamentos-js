12f8daa87c3540b2a5ebec0382842fe207a8c82e
rcchristiane <rcchristiane@gmail.com>
2024-11-05 18:18:23 +0000

    feat(variables): clase sde variables

---
+++ src/clase4/clase-variables.js
@@ -0,0 +1,16 @@
+// Variables descriptivas
+let cajaDeAndy = 'Woody';
+console.log(cajaDeAndy);
+
+// Evitar
+let c = 'Woody';
+let CDA = 'Woody';
+let pcAndy = 'Woody';
+
+// Buena práctica
+let jugueteDeAndy = 'Woody';
+let urlDeUsuario = 'https://una.url.com';
+
+
+// Constantes
+const pi = 3.14;
